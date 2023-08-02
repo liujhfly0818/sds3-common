67c04b1db7ca2815b56fb5abdb441e33fcb9fc8c
liujhfly0818 <liujhfly@163.com>
2023-08-02 23:23:31 +0800

    add @vitejs/plugin-legacy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,6 +18,9 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()],
+    }),
+    legacy({
+      targets: ['defaults', 'not IE 11']
     })
   ],
   resolve: {
